f9cf266abf31c1fdfe1af4f15517ea34522fdb38
Isan Rodriguez Trimiño <isan1989@gmail.com>
2025-04-20 23:35:04 -0700

    Updated Architect, Tech Lead, and TPM PDFs on 2025-04-20

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ md-to-pdf ./architect/Solutions_Architect_Isan_Rodriguez.md
 md-to-pdf ./tech-lead/TechLead_Isan_Rodriguez.md
 md-to-pdf ./tpm/TPM_Isan_Rodriguez.md
 
-DATE =$(date +%Y-%m-%d)
+DATE=$(date +%Y-%m-%d)
 git add .
 git commit -m "Updated Architect, Tech Lead, and TPM PDFs on $DATE"
 git push
